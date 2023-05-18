38c8a76deb8b1e96b0485dba82eb0d38ce486d40
GreenCat <tedd0328@gmail.com>
2023-05-18 18:13:24 -0400

    Started Cell implementation

--- BrainFudge/BrainFudge.js
+++ BrainFudge/BrainFudge.js
@@ -1,30 +1,64 @@
-class Memory {
-    constructor(size){
-        this.size = size;
-        this.arr = Array(size).fill(0)
-        this.index = index;
-        this.arr = arr;
-    }
-}
+//consts
+const CANVAS_HEIGHT = 200;
+const CELL_SIZE = 70;
+const CELL_SEPARATION = 100;
 
-let blocks = new Memory(100);
+//vars
+var cells = [];
+var pointer = 2; //testing: starts at middle cell
+var cellsX;
+var brackets = [];
 
-function myFunction(charcter){
-    if (character === "<"){
-        blocks.index--;
-    }
-    if (character === ">"){
-        blocks.index++;
-    }
-    if (character === "+"){
-        blocks.arr[blocks.index]++;
-    }if (charcter === "-"){
-        blocks.arr[blocks.index]--;
-    } 
-    if (character === "["){
-        continue;
-        //WILL FIGURE OUT LOGIC
-    }if (characer === "]"){
-        continue;
-    }
-}
+var code = []
+var input = []
+var output = ''
+
+function setup() {
+  //setup is run once at the beginning of the sketch.
+  
+  //make canvas
+  createCanvas(windowWidth, CANVAS_HEIGHT)
+  cellsX = width/2
+  
+  //move canvas to middle of screen
+  canvas = select('canvas')
+  canvas.position(0, windowHeight/2-height/2)
+  
+  //just as a test
+  for (let i = 0; i < 5; i++) {
+    cells.push(0)
+  }
+  
+  code = '[++[>++<-].'.split('')
+  print(code);
+}//setup
+
+
+function draw() {
+  //draw is run continuously throughout the program.
+  background('#301c08')
+  
+  //used to test
+  cellsX = mouseX
+  
+  //draw all the cells but like its just one rn
+  for (let i = 0; i < cells.length; i++) {
+    cell = cells[i]
+    
+    //calculate x based on index and a more general x (allows for smooth transitions)
+    let sqx = cellsX + (i - pointer)*CELL_SEPARATION - CELL_SIZE/2
+    
+    //colors
+    noStroke()
+    fill('#915012')
+    
+    //draw square
+    square(sqx, height/2-CELL_SIZE/2, CELL_SIZE, 15)
+  }
+  
+}//draw
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, CANVAS_HEIGHT);
+}//window resize
--- WORKLOG.md
+++ WORKLOG.md
@@ -2,22 +2,22 @@
 
 ## Tedd Lee
 
-## date 05-17-2023
+## 05-17-2023
 
-Found this really useful link to understand how it works; Listed here:
+Found this really useful link to understand how it works; listed here:
 * https://gist.github.com/roachhd/dce54bec8ba55fb17d3a
 
-## date 05-18-2023
+## 05-18-2023
 
-Absent from class was not able to do work IN class due to AP mando exam.
+Worked on UI and began implementing the cell visualization.
 
 
 ## Eduardo Lozano
 
-### date 05-17-2023
+### 05-17-2023
 
-Don't really understand javascript or brainfuck so I had to read the links tedd provided
+Don't really understand javascript or brainfuck so I had to read the links Tedd provided
 
-### date 05-18-2023
+### 05-18-2023
 
 Started implemnting an interpreter for reading brain fuck code. Realized that making a compiler for a langauge may be harder than expected.
