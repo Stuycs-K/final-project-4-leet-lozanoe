57973654733e346f56dce3aadbfdf8337ebad1da
efullmetal <eddieslozano@gmail.com>
2023-05-18 13:49:34 -0400

    added code from lunch

--- BrainFudge/BrainFudge.js
+++ BrainFudge/BrainFudge.js
@@ -1,30 +1,33 @@
-class Memory {
-    constructor(size){
-        this.size = size;
-        this.arr = Array(size).fill(0)
-        this.index = index;
-        this.arr = arr;
-    }
+let index = 0;
+let cells = Array(100).fill(0);
+
+// unshift and shift || push and pop
+
+function parse(string){
+    let loops=[]
+    for(let i=0; i<string.length; i++){
+        if (character === "<"){
+            index--;
+        }
+        if (character === ">"){
+            index++;
+        }
+        if (character === "+"){
+            cells[index]++;
+        }if (charcter === "-"){
+            cells[index]--;
+        } 
+        if (character === "["){
+            loops.shift(index);
+            //WILL FIGURE OUT LOGIC
+        }if (characer === "]"){
+            loops.push(index);
+            if(cells[index]!=0){
+                i = index;
+            }else{
+                loops.unshift();
+                loops.pop();
+            }
+        }
 }
-
-let blocks = new Memory(100);
-
-function myFunction(charcter){
-    if (character === "<"){
-        blocks.index--;
-    }
-    if (character === ">"){
-        blocks.index++;
-    }
-    if (character === "+"){
-        blocks.arr[blocks.index]++;
-    }if (charcter === "-"){
-        blocks.arr[blocks.index]--;
-    } 
-    if (character === "["){
-        continue;
-        //WILL FIGURE OUT LOGIC
-    }if (characer === "]"){
-        continue;
-    }
 }
