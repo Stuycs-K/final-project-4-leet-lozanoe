69476cdca11d411d8fd6d8fd568e29ed3d6e63c5
efullmetal <eddieslozano@gmail.com>
2023-05-25 11:02:02 -0400

    Did parser to see if brackets line up

--- BrainFudge/Parser.js
+++ BrainFudge/Parser.js
@@ -29,5 +29,25 @@ function parse(ch) {
 }
 
 function checkInput() {
-  return true; // false if brackets don't align or something
+  let num = 0
+  let errorMessage = ""
+  inpString = codeInput.value()
+  for (let i = 0; i < inpString.length; i++){
+    if (inpString[i] === "["){
+      num++;
+    }if (inpString[i] === "]"){
+      num--;
+    }
+  }
+  if (num === 0){
+    return true;
+  }else{
+    if (num > 0){
+      errorMessage = `There are ${num} excess brackets of [`;
+    }
+    if (num < 0){
+      errorMessage = `There are ${num} excess brackets of ]`;
+    }
+    return false
+  }
 }
